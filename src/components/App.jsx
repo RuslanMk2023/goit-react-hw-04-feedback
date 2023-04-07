71a105a04a7c9130eaa28093dfed16abb8811d86
RuslanMischuk <dobertorton@gmail.com>
2023-04-07 14:06:13 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -43,7 +43,7 @@ export class App extends Component {
         </Section>
 
         <Section title="Statistics:">
-          {!(this.countTotalFeedback() == 0) ? (
+          {!(this.countTotalFeedback() === 0) ? (
             <Statistics
               good={good}
               neutral={neutral}
